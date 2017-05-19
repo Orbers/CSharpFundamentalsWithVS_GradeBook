ba4098803f93ea7f4cae5fc2657626b01275bf5e
Corbin Seghers <corbin.seghers@choosebroadspire.com>
2017-05-19 10:41:13 -0500

    The Grade Book sub-section complete

--- Csharp_Funadamentals_w_VS2015/Program.cs
+++ Csharp_Funadamentals_w_VS2015/Program.cs
@@ -10,6 +10,32 @@ namespace Csharp_Funadamentals_w_VS2015
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+
+            if(hoursOfSleep >= 8)
+            {
+                Console.WriteLine("you got plenty of sleep");
+            }
+            else
+            {
+                Console.WriteLine("you should sleep more");
+            }
+
+
+
+            Console.WriteLine("Hello " + name);
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
